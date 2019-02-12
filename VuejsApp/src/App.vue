161320256854cfada422a672938aabb53c4c2895
jamiepollock <jamie.does.umbraco@gmail.com>
2019-02-11 20:17:23 -0600

    Bulma-fied App & Home vues.

--- VuejsApp/src/App.vue
+++ VuejsApp/src/App.vue
@@ -1,6 +1,23 @@
 <template>
     <div id="app">
-        <Home msg="People" v-bind:mediaAssetsRootUrl="mediaAssetsRootUrl" />
+        <header class="hero is-primary">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">GraphQL for Umbraco Demo</h1>
+                    <h2 class="subtitle">Featuring VueJS &amp; Apollo!</h2>
+                </div>
+            </div>
+        </header>
+        <main class="section">
+            <div class="container">
+                <Home v-bind:mediaAssetsRootUrl="mediaAssetsRootUrl" />
+            </div>
+        </main>
+        <footer class="footer">
+            <div class="container">
+                <p>Welcome to your new single-page application, built with <a href="https://vuejs.org" target="_blank">Vue.js</a>.</p>
+            </div>
+        </footer>
     </div>
 </template>
 
--- VuejsApp/src/components/Home.vue
+++ VuejsApp/src/components/Home.vue
@@ -2,7 +2,6 @@
     <div class="home">
         <h1 class="is-size-1">Home</h1>
         <People v-bind:items="allPeople" />
-        <p>Welcome to your new single-page application, built with <a href="https://vuejs.org" target="_blank">Vue.js</a>.</p>
     </div>
 </template>
 
