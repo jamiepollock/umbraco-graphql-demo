f10cb56937337897c8eca29b2976b831618df469
jamiepollock <jamie.does.umbraco@gmail.com>
2019-02-11 20:16:46 -0600

    Added static title to People component

--- VuejsApp/src/components/People.vue
+++ VuejsApp/src/components/People.vue
@@ -1,5 +1,6 @@
 ﻿<template>
     <section class="people">
+        <h2 class="is-size-2">People</h2>
         <div class="columns is-multiline" v-if="items.length > 0">
             <div class="column is-half" v-for="person in items" v-bind:key="person.id">
                 <Person v-bind:name="person.name" v-bind:photoUrl="person.photoUrl" />
