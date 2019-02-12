7c5cdacf466133b20e59f6ba6a5401c05676234f
jamiepollock <jamie.does.umbraco@gmail.com>
2019-02-11 18:53:50 -0600

    Added People component

---
+++ VuejsApp/src/components/People.vue
@@ -0,0 +1,22 @@
+﻿<template>
+    <section class="people">
+        <ul v-if="items.length > 0">
+            <li v-for="person in items" v-bind:key="person._contentData.id">
+                {{person._contentData.name}}
+            </li>
+        </ul>
+        <p v-if="!items">Nobody here!</p>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'People',
+        props: {
+            items: Array
+        }
+    };
+</script>
+
+<style scoped>
+</style>
