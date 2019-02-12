95be2c601bce592cb06d3363399393f7862c0154
jamiepollock <jamie.does.umbraco@gmail.com>
2019-02-11 20:15:44 -0600

    Removed msg prop in favour of a static title for Home

--- VuejsApp/src/components/Home.vue
+++ VuejsApp/src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home">
-        <h1>{{ msg }}</h1>
+        <h1 class="is-size-1">Home</h1>
         <People v-bind:items="allPeople" />
         <p>Welcome to your new single-page application, built with <a href="https://vuejs.org" target="_blank">Vue.js</a>.</p>
     </div>
@@ -13,7 +13,6 @@
     export default {
         name: 'Home',
         props: {
-            msg: String,
             mediaAssetsRootUrl: String
         },
         data() {
